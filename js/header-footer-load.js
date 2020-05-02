95f2ecf6876b30ec1e3c5fd425f10c714f2016fb
hafizmuhammadusman537 <m.usman537@gmail.com>
2020-05-03 02:30:46 +0500

    separate header and footer

--- js/header-footer-load.js
+++ js/header-footer-load.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
-    $("#load-header").load("../header.html");
-    $("#load-footer").load("../footer.html");
+    $("#load-header").load("header.html");
+    $("#load-footer").load("footer.html");
 });
